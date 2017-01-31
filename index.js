1c3e72e53372582f57dd50231260ba3535a34f57
phelpsinc <chuck@phelpsinc.net>
2017-01-30 21:22:10 -0600

    Commit for class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'; var
 APP_ID = undefined;
 var AlexaSkill = require('AlexaSkill');
+var SPEECH_OUTPUT = 'Hello';
 
 var GreeterService = function() {
     AlexaSkill.call(this, APP_ID);
