e6d56164a0a04103f8513737c9ac6833201bcd59
phelpsinc <chuck@phelpsinc.net>
2017-02-01 21:04:11 -0600

    Woo

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // Version 0.1 
 // Credit to Big Nerd Ranch
 /* */
-'use strict'; var
-var APP_ID = amzn1.ask.skill.4f5d93b8-4800-4fb6-9a57-92566010ca1c;
+'use strict'; //var
+var APP_ID = 'amzn1.ask.skill.4f5d93b8-4800-4fb6-9a57-92566010ca1c';
 var AlexaSkill = require('AlexaSkill');
 var SPEECH_OUTPUT = 'Chenchu is the man of steel';
 
@@ -21,9 +21,9 @@ GreeterService.prototype.eventHandlers.onLaunch = helloResponseFunction;
 
 GreeterService.prototype.intentHandlers = {
     'HelloWorldIntent': helloResponseFunction
-}
+};
 
-exports.handler = fucntion(event, context) {
+exports.handler = function(event, context) {
     var greeterService = new GreeterService();
     greeterService.execute(event, context);
 };
